41ee205f21e06a9c469f4d4c675efa5117826ac0
Jan Armbrust <jan.armbrust@googlemail.com>
2021-04-10 20:55:02 +0200

    reformate

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
-import React from "react";
+import React from 'react'
+import { Introduction } from './components/Introduction'
 
 function App() {
   return (
-    <div className="App">
-      Hello World
-      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
-        <div className="flex-shrink-0"></div>
-        <div>
-          <div className="text-xl font-medium text-black">ChitChat</div>
-          <p className="text-gray-500">You have a new message!</p>
-        </div>
-      </div>
+    <div>
+      <Introduction />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
