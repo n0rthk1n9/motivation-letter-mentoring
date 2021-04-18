725dada8ba5b82bdb45b70760f67ade8cbdc1c11
Jan Armbrust <jan.armbrust@googlemail.com>
2021-04-18 22:23:01 +0200

    add products component

---
+++ src/components/Products/Products.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+export function Products(): JSX.Element {
+  return (
+    <div className="flex flex-col md:flex-row p-2 md:p-6 max-w mx-2 md:mx-20 my-2 md:my-20 rounded-xl shadow-md items-center bg-fawn">
+      <div>Produkt</div>
+      <div>Produkt</div>
+      <div>Produkt</div>
+    </div>
+  )
+}
