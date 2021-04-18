203f01801819e7748ccd35e9ddbb59e92f377c65
Jan Armbrust <jan.armbrust@googlemail.com>
2021-04-18 23:09:08 +0200

    add customer feedback card component

---
+++ src/components/CustomerFeedbackCard/CustomerFeedbackCard.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react'
+
+interface ProductCardProps {
+  img: string
+  content: string
+}
+
+export function CustomerFeedbackCard(props: ProductCardProps): JSX.Element {
+  const { img, content } = props
+
+  return (
+    <div className="bg-peach-crayola rounded-lg shadow-md p-2 md:p-6 flex space-x-2 md:space-x-5">
+      <img className="h-12 rounded-full" src={img} />
+      <div>{content}</div>
+    </div>
+  )
+}
