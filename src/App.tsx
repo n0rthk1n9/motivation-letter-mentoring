d74c6041e93e2fae5600dc9f032304326029d38a
Jan Armbrust <jan.armbrust@googlemail.com>
2021-04-18 21:42:04 +0200

    add and use motivation component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Introduction } from './components/Introduction'
+import { Motivation } from './components/Motivation'
 
 function App() {
   return (
     <div>
       <Introduction />
+      <Motivation />
     </div>
   )
 }
---
+++ src/components/Motivation/Motivation.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react'
+
+export function Motivation(): JSX.Element {
+  return (
+    <div className="p-2 md:p-6 max-w mx-2 md:mx-20 my-2 md:my-20 rounded-xl shadow-md items-center bg-fawn space-y-2 md:space-y-5">
+      <h1>KEINE ANGST MEHR VOR MOTIVATIONSSCHREIBEN</h1>
+      <p>
+        Die Wissenschaftlerin in mir fing an nach Möglichkeiten zu suchen eine
+        Struktur in Motivationsschreiben zu finden und eine Methodik zu
+        entwickeln, um Motivationsschreiben zu erstellen die einen glücklich
+        machen! Mit meinen Beratungsgesprächen sowie mit meiner How-To
+        Motivationsschreiben Anleitung möchte ich euch die Angst vor der
+        Erstellung von Motivationsschreiben nehmen, EURE SELBSTVERTRAUEN IN EURE
+        FÄHIGKEITEN STÄRKEN UND EUCH ERMUTIGEN EURE HERZENSWÜNSCHE ZU VERFOLGEN!
+        Und ja, ich habe im Bachelor ein Auslandssemesterstipendium gewonnen und
+        dann im Master sogar nochmal.
+      </p>
+    </div>
+  )
+}
