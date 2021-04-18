7f6cf9ddde6a1d8988c8549940bf02e76269836c
Jan Armbrust <jan.armbrust@googlemail.com>
2021-04-18 21:54:50 +0200

    use subscribe form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Introduction } from './components/Introduction'
 import { Motivation } from './components/Motivation'
+import { SubscribeForm } from './components/SubscribeForm'
 
 function App() {
   return (
     <div>
       <Introduction />
       <Motivation />
+      <SubscribeForm />
     </div>
   )
 }
